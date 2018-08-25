ea126b34b3a9d0ce6dd4a04513ee5c357a7e5118
Anton Desin <anton.desin@gmail.com>
2018-08-25 15:41:47 +0300

    Первый коммит

---
+++ src/Result.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Result
+ *
+ *
+ * @author Anton Desin anton.desin@gmail.com
+ * @copyright (c) Anton Desin
+ * @link https://desin.name
+ */
+
+namespace Desin;
+
+
+class Result {
+	
+	/**
+	 * @var array Массив сообщений об ошибках
+	 */
+	public $error = array();
+	
+	/**
+	 * @var array|null Данные результата
+	 */
+	public $data = null;
+	
+	/**
+	 * @var boolean Статус результата
+	 */
+	public $success = false;
+	
+	/**
+	 * Метод проверяет, имеет ли результат статус "Успешно"
+	 *
+	 * @return boolean
+	 */
+	public function isSuccess(){
+		return $this->success;
+	}
+	
+	/**
+	 * Метод проверяет, имеет ли результат статус "Ошибка"
+	 *
+	 * @return boolean
+	 */
+	public function isError(){
+		return !empty($this->error);
+	}
+	
+	/**
+	 * Установка статуса "Успешно"
+	 *
+	 * @param array|null $data Данные результата
+	 * @return void
+	 */
+	public function setSuccess($data = null){
+		$this->success = true;
+		if($data) $this->data = $data;
+	}
+	
+	/**
+	 * Установка статуса "Ошибка"
+	 *
+	 * @param string $message Сообщение об ошибке
+	 */
+	public function setError($message){
+		$this->success = false;
+		$this->error[] = $message;
+	}
+	
+	public function setData($data){
+		if($data) $this->data = $data;
+	}
+	
+	/**
+	 * Метод возвращает текст последней ошибки
+	 *
+	 * @return string Текст ошибки
+	 */
+	public function lastError(){
+		$strError = end($this->error);
+		if(!$strError) $strError = '';
+		return $strError;
+	}
+	
+	/**
+	 * Метод возвращает все имеющиеся ошибки
+	 *
+	 * @param boolean $string Если параметр равен true, то метод вернёт ошибкт в виде строки, через запятую
+	 * @return array|string
+	 */
+	public function getErrors($string=false){
+		if($string){
+			return @implode(', ', $this->error);
+		}else{
+			return $this->error;
+		}
+	}
+	
+	/**
+	 * Вернуть содержимое результата в виде массива вида:
+	 *	    array(
+	 *	        'success' => true|false,
+	 *	        'message' => ...,
+	 *	        'data' => ...
+	 *	    )
+	 *
+	 * @return array Содержимое результата
+	 */
+	public function getArray(){
+		$result = array(
+			'success' => $this->success,
+			'message' => $this->lastError(),
+			'data' => $this->data,
+		);
+		
+		return $result;
+	}
+	
+	/**
+	 * Вернуть содержимое результата в формате JSON
+	 * Содержимое JSON аналогично результату, возвращаемому методом \Bureau\Site\Result::getArray()
+	 *
+	 * @return type
+	 */
+	public function getJSON(){
+		return json_encode($this->getArray());
+	}
+}
